201824ad1782a9b4610cb2d93abc0342928e36dc
Juliana Farias <juliana.ferreira.farias@gmail.com>
2021-03-30 16:08:11 -0300

    Projeto Criado

---
+++ src/main/java/com/julianafarias/cursomcjava/CursomcjavaApplication.java
@@ -0,0 +1,13 @@
+package com.julianafarias.cursomcjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julianafarias/cursomcjava/CursomcjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianafarias.cursomcjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
