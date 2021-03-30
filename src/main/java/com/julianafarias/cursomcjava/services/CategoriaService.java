5b6f1246312ceb33546c5a91f2512ada86fe6bf6
Juliana Farias <juliana.ferreira.farias@gmail.com>
2021-03-30 18:47:17 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/julianafarias/cursomcjava/services/CategoriaService.java
+++ src/main/java/com/julianafarias/cursomcjava/services/CategoriaService.java
@@ -1,13 +1,14 @@
 package com.julianafarias.cursomcjava.services;
 
-import java.util.List;
 import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.julianafarias.cursomcjava.domain.Categoria;
 import com.julianafarias.cursomcjava.repositories.CategoriaRepository;
+import com.julianafarias.cursomcjava.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,11 +16,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
-		
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-
-	}
+	public Categoria buscar(Integer id) { 
+		 Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+		}
 
 }
