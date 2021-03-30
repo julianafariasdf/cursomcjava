d9fd72921c5622eed2177cca2233d69cd5e733fc
Juliana Farias <juliana.ferreira.farias@gmail.com>
2021-03-30 17:59:31 -0300

    Criando operacao de instanciacao

--- src/main/java/com/julianafarias/cursomcjava/CursomcjavaApplication.java
+++ src/main/java/com/julianafarias/cursomcjava/CursomcjavaApplication.java
@@ -1,13 +1,33 @@
 package com.julianafarias.cursomcjava;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.julianafarias.cursomcjava.domain.Categoria;
+import com.julianafarias.cursomcjava.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcjavaApplication {
+public class CursomcjavaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcjavaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
